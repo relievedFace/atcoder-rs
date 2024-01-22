61600027b6fa4a1bcf74469d213652b7c8261548
Mikihiro Okuwa <relievedFace@gmail.com>
2024-01-22 22:06:26 +0900

    ABC336-Cを解いた

--- abc336/src/bin/c.rs
+++ abc336/src/bin/c.rs
@@ -3,6 +3,26 @@
 
 use proconio::input;
 
+const EVEN: [char; 5] = ['0', '2', '4', '6', '8'];
+
 fn main() {
-    input! {}
+    input! {
+        mut n: usize,
+    }
+
+    let mut ans = String::new();
+
+    n -= 1;
+
+    while n != 0 {
+        ans.push(EVEN[n % 5]);
+        n /= 5;
+    }
+
+    let mut ans: String = ans.chars().rev().collect();
+
+    if ans.is_empty() {
+        ans.push('0');
+    }
+    println!("{}", ans);
 }
