f7a039645f64954dbe6d566f5030cef46b90bfbf
Mikihiro Okuwa <relievedFace@gmail.com>
2024-01-27 17:50:26 +0900

    ABC337-Aを解いた

--- abc337/src/bin/a.rs
+++ abc337/src/bin/a.rs
@@ -4,5 +4,19 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        xy: [(i32, i32); n],
+    }
+
+    let xの合計: i32 = xy.iter().map(|(x, _)| x).sum();
+    let yの合計: i32 = xy.iter().map(|(_, y)| y).sum();
+
+    let 答え = match xの合計.cmp(&yの合計) {
+        std::cmp::Ordering::Less => "Aoki",
+        std::cmp::Ordering::Equal => "Draw",
+        std::cmp::Ordering::Greater => "Takahashi",
+    };
+
+    println!("{}", 答え)
 }
