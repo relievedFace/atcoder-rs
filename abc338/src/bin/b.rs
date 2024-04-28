13b40fca8193bd7af6b06e806e0b73fd3852c369
Mikihiro Okuwa <relievedFace@gmail.com>
2024-04-29 00:03:26 +0900

    ABC338-Bを解いた

--- abc338/src/bin/b.rs
+++ abc338/src/bin/b.rs
@@ -1,8 +1,31 @@
 #![allow(non_snake_case)]
 #![allow(unused_variables)]
 
-use proconio::input;
+use proconio::{input, marker::Chars};
+use std::{cmp::Reverse, collections::HashMap, hash::Hash};
 
 fn main() {
-    input! {}
+    input! {
+        s: Chars,
+    }
+
+    let map = convert_vec_to_hashmap(&s);
+
+    let ans = ('a'..='z')
+        .map(|c| (c, map.get(&c).unwrap_or(&0)))
+        .max_by_key(|k| (k.1, Reverse(k.0)))
+        .unwrap()
+        .0;
+
+    println!("{}", ans);
+}
+
+pub fn convert_vec_to_hashmap<T>(vec: &[T]) -> HashMap<T, usize>
+where
+    T: Clone + Eq + Hash,
+{
+    vec.iter().cloned().fold(HashMap::new(), |mut acc, x| {
+        *acc.entry(x).or_insert(0) += 1;
+        acc
+    })
 }
