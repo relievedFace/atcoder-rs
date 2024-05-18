1704fc52e94eb3f1605a5238301ca49aed104f96
Mikihiro Okuwa <relievedFace@gmail.com>
2024-05-18 10:14:58 +0900

    ABC353-Bを解いた

--- abc353/src/bin/b.rs
+++ abc353/src/bin/b.rs
@@ -4,5 +4,27 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: i32,
+        a: [i32; n],
+    }
+
+    let mut attraction = 0;
+    let mut ans = 0;
+
+    for a in a {
+        if attraction + a > k {
+            attraction = a;
+            ans += 1;
+        } else {
+            attraction += a;
+        }
+    }
+
+    if attraction != 0 {
+        ans += 1;
+    }
+
+    println!("{}", ans);
 }
